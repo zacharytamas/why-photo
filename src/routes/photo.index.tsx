bc3638d42c83e61a92ef21446828d43f28bba7cb
Zachary <zachary@tamas.sh>
2025-09-17 09:18:24 -0400

    Use the same photo UI on the photo index page
    This page for now shows a random asset

--- src/routes/photo.index.tsx
+++ src/routes/photo.index.tsx
@@ -1,7 +1,8 @@
-import { PhotoThumbnail } from '@/components/PhotoThumbnail'
-import type { Asset } from '@/models/immich/Asset'
+import { ThumbnailGrid } from '@/components/ThumbnailGrid'
+import { useAssetWithNearby } from '@/hooks/useAssetWithNearby'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { Loader2 } from 'lucide-react'
 
 export const Route = createFileRoute('/photo/')({
   component: RouteComponent,
@@ -12,32 +13,46 @@ function RouteComponent() {
     queryKey: ['randomPhoto'],
     queryFn: () => fetch('/api/immich/randomPhoto').then((res) => res.json()),
   })
+  const { asset, nearbyPhotos, error, isLoading } = useAssetWithNearby(
+    randomAsset?.id,
+  )
 
-  const { data: nearbyPhotos } = useQuery({
-    queryKey: ['nearbyPhotos', randomAsset?.id],
-    enabled: !!randomAsset?.id,
-    queryFn: () =>
-      fetch(`/api/immich/nearbyPhotos/${randomAsset?.id}`).then((res) =>
-        res.json(),
-      ),
-  })
-
-  return (
-    <div className="flex w-full gap-4">
-      <div className="flex-1">
-        <img
-          src={`/api/immich/viewAsset/${randomAsset?.id}`}
-          alt="Asset"
-          className="h-[800px] object-cover"
-        />
-      </div>
-      <div className="flex-1">
-        <div className="grid grid-cols-3 gap-2">
-          {nearbyPhotos?.map((photo: Asset) => (
-            <PhotoThumbnail key={photo.id} photo={photo} />
-          ))}
+  if (error) {
+    return (
+      <div className="flex items-center justify-center w-full h-[800px]">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+            Asset Not Found
+          </h2>
+          <p className="text-gray-600">
+            The requested photo could not be found.
+          </p>
         </div>
       </div>
-    </div>
+    )
+  }
+
+  return (
+    <main className="flex flex-1 gap-4 p-4">
+      <div className="flex-1 flex">
+        {isLoading ? (
+          <div className="flex items-center justify-center w-full h-full">
+            <Loader2 className="w-32 animate-spin" />
+          </div>
+        ) : (
+          <img
+            src={`/api/immich/viewAsset/${randomAsset.id}`}
+            alt="Asset"
+            className="object-scale-down w-full aspect-square"
+          />
+        )}
+      </div>
+      <div className="flex flex-col flex-1 gap-4">
+        <div className="overflow-y-auto flex-1">
+          {<ThumbnailGrid photos={nearbyPhotos} />}
+        </div>
+        <div className="flex-2">Composer</div>
+      </div>
+    </main>
   )
 }
